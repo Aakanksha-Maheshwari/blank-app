27be655f877d7ce5ade1585ea81bb037a808a841
Aakanksha-Maheshwari <aamahesh@syr.edu>
2024-09-26 14:04:39 +0000

    streamlitchanges

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,10 @@ lab1_page = st.Page("lab1.py", title="lab1")
 lab2_page = st.Page("lab2.py", title="lab2")
 lab3_page = st.Page("lab3.py", title ="lab3")
 lab4_page = st.Page("lab4.py", title ="lab4")
+lab5_page = st.Page("lab5.py", title ="lab5")
 
 # Initialize navigation with the pages
-pg = st.navigation([lab1_page,lab2_page,lab3_page,lab4_page])
+pg = st.navigation([lab1_page,lab2_page,lab3_page,lab4_page,lab5_page])
 
 # Set page configuration (optional but helps with page title and icon)
 st.set_page_config(page_title="Lab Manager", page_icon=":memo:")
